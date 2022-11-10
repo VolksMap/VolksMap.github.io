de413380b9aa8eba824c41010ab9919825175b12
VolksMap <116445493+VolksMap@users.noreply.github.com>
2022-11-10 20:38:03 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 function img01 () {
-    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1F80_4k14xid-QT1G3BxDztm3FLYlTNU&ehbc=2E312F";
+    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1OGX9AwJd9N4vreC1m8cBs3lhIcj79M0&ehbc=2E312F";
 }
 function img02 () {
-    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1c5I2EkBCB4zCa2-eS0K2-UB2kurq960&ehbc=2E312F";
+    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1F80_4k14xid-QT1G3BxDztm3FLYlTNU&ehbc=2E312F";
 }
 function img03 () {
-    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=12CXXqWLBiBkWRvZ5EEJC2gjSHvyiQpI&ehbc=2E312F";
+    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1qHq7YE8IUyN-tDyf1mI20JRey3yyI0s&ehbc=2E312F";
 }
 
 
