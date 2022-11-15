84cf0e0d5ae72ebeb9c751a9341eb126ff67b227
VolksMap <116445493+VolksMap@users.noreply.github.com>
2022-11-15 19:26:58 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,14 +2,29 @@ function img01 () {
     document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1OGX9AwJd9N4vreC1m8cBs3lhIcj79M0&ehbc=2E312F";
 }
 function img02 () {
-    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1F80_4k14xid-QT1G3BxDztm3FLYlTNU&ehbc=2E312F";
+    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1qHq7YE8IUyN-tDyf1mI20JRey3yyI0s&ehbc=2E312F";
 }
 function img03 () {
-    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1qHq7YE8IUyN-tDyf1mI20JRey3yyI0s&ehbc=2E312F";
+    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1F80_4k14xid-QT1G3BxDztm3FLYlTNU&ehbc=2E312F";
 }
 
 
 $(".sidebar ul li").on('click' , function() {
     $(".sidebar ul li.active").removeClass('active');
     $(this).addClasss('active');
+})
+
+var dropop = document.querySelector('span.arrow')
+var dropdown = document.querySelector('ul.dropdown-menu-dark')
+var cont = 0;
+dropop.addEventListener('click', function(){
+   if(cont == 0){
+    dropdown.classList.add('see');
+    dropop.classList.add('see');
+    cont +=1;
+   }else{
+    dropdown.classList.remove('see');
+    dropop.classList.remove('see');
+    cont = 0;
+   }
 })
