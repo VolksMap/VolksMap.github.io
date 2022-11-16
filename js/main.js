cba4450c9342ef069280e9dec4d0578e5e9c75fd
Marcos Consani <48033053+makazunix@users.noreply.github.com>
2022-11-15 23:08:59 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function img01 () {
-    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1OGX9AwJd9N4vreC1m8cBs3lhIcj79M0&ehbc=2E312F";
+    document.getElementById("stlgeral").src="https://www.google.com/maps/d/u/0/embed?mid=13JbxNc5Bo7_aSuAjPTlUVKi3uJMzFYA&ehbc=2E312F";
 }
 function img02 () {
-    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1qHq7YE8IUyN-tDyf1mI20JRey3yyI0s&ehbc=2E312F";
+    document.getElementById("stlgeral").src="https://www.google.com/maps/d/u/0/embed?mid=13JbxNc5Bo7_aSuAjPTlUVKi3uJMzFYA&ehbc=2E312F";
 }
 function img03 () {
     document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1F80_4k14xid-QT1G3BxDztm3FLYlTNU&ehbc=2E312F";
