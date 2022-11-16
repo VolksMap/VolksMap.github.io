fcf6198d7c85f5dd7a298dbf06efce1d2c90dfea
VolksMap <116445493+VolksMap@users.noreply.github.com>
2022-11-16 11:58:38 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function img01 () {
     document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1OGX9AwJd9N4vreC1m8cBs3lhIcj79M0&ehbc=2E312F";
 }
 function img02 () {
-    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1qHq7YE8IUyN-tDyf1mI20JRey3yyI0s&ehbc=2E312F";
+    document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1YGekpz0sDJ_xj5SprznubKRHGwe-008&ehbc=2E312F";
 }
 function img03 () {
     document.getElementById("stlgeral").src="https://www.google.com/maps/d/embed?mid=1F80_4k14xid-QT1G3BxDztm3FLYlTNU&ehbc=2E312F";
